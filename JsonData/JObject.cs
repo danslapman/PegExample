0c428e96272ac94cdb6ebdc27ea057896b2ade20
Daniil Smirnov <danslapman@gmail.com>
2015-10-15 00:01:48 +0300

    Add JsonData project

---
+++ JsonData/JObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace JsonData
+{
+    public abstract class JObject
+    {
+        public class Object : JObject
+        {
+            public Dictionary<string, JObject> Properties { get; set; } 
+        }
+
+        public class Array : JObject
+        {
+            public List<JObject> Items { get; set; } 
+        }
+
+        public class Value : JObject
+        {
+            public JValue Data { get; set; }
+        }
+
+        public static Object CreateObject()
+        {
+            return new Object {Properties = new Dictionary<string, JObject>()};
+        }
+
+        public static Array CreateArray()
+        {
+            return new Array {Items = new List<JObject>()};
+        }
+
+        public static Value CreateValue(JValue value)
+        {
+            return new Value {Data = value};
+        }
+    }
+}
---
+++ JsonData/JValue.cs
@@ -0,0 +1,25 @@
+﻿namespace JsonData
+{
+    public abstract class JValue
+    {
+        public class Null : JValue
+        {
+            
+        }
+
+        public class String : JValue 
+        {
+            public string Value { get; set; }
+        }
+
+        public static JValue CreateNull()
+        {
+            return new Null();
+        }
+
+        public static JValue CreateString(string value)
+        {
+            return new String {Value = value};
+        }
+    }
+}
